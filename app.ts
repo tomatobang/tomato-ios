bd96f9c99f5d7c26f4260806bbd9605a8ac666a1
kobepeng <pengkobe123@gmail.com>
2019-11-14 09:07:55 +0800

    build: make it runnable

--- app.ts
+++ app.ts
@@ -1,12 +1,17 @@
 'use strict';
 
-module.exports = app => {
+import * as path from 'path';
+import { Application } from 'egg';
+export default (app: Application) => {
   /**
    * 应用启动前初始化工作
    */
   app.beforeStart(async () => {
-    app['tips'] = 'tomato-user start...';
+    app['tips'] = 'tomato-todo start...';
     console.log(app['tips']);
-  });
 
+    // 删除 socket 所有连接;处理未完成的番茄钟( TODO )
+  });
+  const directory = path.join(app.config.baseDir, 'app/util');
+  app.loader.loadToApp(directory, 'util');
 };
--- config/config.prod.ts
+++ config/config.prod.ts
@@ -25,8 +25,3 @@ exports.mongoose = {
   url: 'mongodb://' + env.DATABASE_MONGODB_USERNAME_PASSWORD + '@' + env.DATABASE_MONGODB_HOST_PORT + '/tomatobang',
   options: {},
 };
-
-exports.serverPort = {
-  serverPort: env.serverPort || 3000,
-};
-
